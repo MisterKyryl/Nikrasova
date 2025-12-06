e89e74ead3c9c62c6257767bb7a39d16d35b7a78
MisterKyryl <kosak.kirilll543@gmail.com>
2025-12-06 13:38:31 +0200

    Updates

--- js/dev/app.js
+++ js/dev/app.js
@@ -479,10 +479,6 @@ updateMobileAttr();
 window.addEventListener("resize", updateMobileAttr);
 document.addEventListener("DOMContentLoaded", () => {
   const html = document.documentElement;
-  window.addEventListener(
-    "scroll",
-    () => html.toggleAttribute("data-fls-header-scroll-active", window.scrollY > 0)
-  );
   const observer = new MutationObserver(() => {
     if (innerWidth >= 768) {
       html.toggleAttribute(
@@ -495,6 +491,38 @@ document.addEventListener("DOMContentLoaded", () => {
     (el) => observer.observe(el, { attributes: true, attributeFilter: ["class"] })
   );
 });
+function headerScroll() {
+  const header = document.querySelector("[data-fls-header-scroll]");
+  const headerShow = header.hasAttribute("data-fls-header-scroll-show");
+  const headerShowTimer = header.dataset.flsHeaderScrollShow ? header.dataset.flsHeaderScrollShow : 500;
+  const startPoint = header.dataset.flsHeaderScroll ? header.dataset.flsHeaderScroll : 1;
+  let scrollDirection = 0;
+  let timer;
+  document.addEventListener("scroll", function(e) {
+    const scrollTop = window.scrollY;
+    clearTimeout(timer);
+    if (scrollTop >= startPoint) {
+      !header.classList.contains("--header-scroll") ? header.classList.add("--header-scroll") : null;
+      if (headerShow) {
+        if (scrollTop > scrollDirection) {
+          header.classList.contains("--header-show") ? header.classList.remove("--header-show") : null;
+        } else {
+          !header.classList.contains("--header-show") ? header.classList.add("--header-show") : null;
+        }
+        timer = setTimeout(() => {
+          !header.classList.contains("--header-show") ? header.classList.add("--header-show") : null;
+        }, headerShowTimer);
+      }
+    } else {
+      header.classList.contains("--header-scroll") ? header.classList.remove("--header-scroll") : null;
+      if (headerShow) {
+        header.classList.contains("--header-show") ? header.classList.remove("--header-show") : null;
+      }
+    }
+    scrollDirection = scrollTop <= 0 ? 0 : scrollTop;
+  });
+}
+document.querySelector("[data-fls-header-scroll]") ? window.addEventListener("load", headerScroll) : null;
 class DynamicAdapt {
   constructor() {
     this.type = "min";
